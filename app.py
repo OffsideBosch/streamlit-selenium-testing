ad88899506fcead14c5a33061fac74284e7814a0
OffsideBosch <mroffsidebosch@gmail.com>
2025-01-09 01:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import json
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import sys
+sys.path.append('./amdcommunity.py')
 from amdcommunity import AmdCommunity
  # Import your AmdCommunity class
 
