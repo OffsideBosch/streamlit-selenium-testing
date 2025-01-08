a70ddf67f0d21d3398e0ce4edb24eb1a93f2198c
OffsideBosch <mroffsidebosch@gmail.com>
2025-01-09 01:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 import streamlit as st
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--diable-dve-shm-uage')
+driver = webdriver.Chrome(options=options)
 
-    driver = get_driver()
-    driver.get("https://forums.tomshardware.com/threads/how-to-ask-for-troubleshooting-help-v2.516288/")
-
-    st.code(driver.page_source)
+driver.get("https://community.amd.com/t5/pc-processors/cpu-overheating-mystery/td-p/679185")
+st.code(driver.page_source)
