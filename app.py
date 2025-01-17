9f0da2b1d4e5d6ee50b685e4953ebf13443b6b10
OffsideBosch <mroffsidebosch@gmail.com>
2025-01-17 21:28:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import sys
 import os
 import sys
+import json
 sys.path.append('./amdcommunity.py')
 from amdcommunity import AmdCommunity
  # Import your AmdCommunity class
@@ -15,13 +16,14 @@ st.title("Testing")
 
 # Text input for URL
 url = st.text_input("Enter the AMD Community URL:", "")
-
 # Process the URL and display the result
 if st.button("Get Data"):
     if url.strip():
         try:
             # Retrieve and process the data
             result = amd_community.get_and_process_data([url])
+            with open("temp.json","w") as f:
+             json.dump(result,f,indent=3)
             
             # Display the JSON result
             st.code(json.dumps(result, indent=4), language="json")
