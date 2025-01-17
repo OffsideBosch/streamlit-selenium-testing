72e7ef9ab5c5962c265c55d05c561de24ae77624
OffsideBosch <mroffsidebosch@gmail.com>
2025-01-17 21:33:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,17 +18,10 @@ st.title("Testing")
 url = st.text_input("Enter the AMD Community URL:", "")
 # Process the URL and display the result
 if st.button("Get Data"):
-    if(os.path.exists("temp.json")):
-     print("false path")
     if url.strip():
         try:
             # Retrieve and process the data
             result = amd_community.get_and_process_data([url])
-            with open("temp.json","w") as f:
-             json.dump(result,f,indent=3)
-            f.close()
-            if(os.path.exists("temp.json")):
-             print("true path exists")
             
             # Display the JSON result
             st.code(json.dumps(result, indent=4), language="json")
