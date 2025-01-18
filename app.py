0a52d5f1d85b03c75d9d3daa0950e55a897580d8
OffsideBosch <mroffsidebosch@gmail.com>
2025-01-18 13:29:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 import os
 import sys
 import json
+import tempfile
 sys.path.append('./amdcommunity.py')
 from amdcommunity import AmdCommunity
  # Import your AmdCommunity class
@@ -11,14 +12,30 @@ from amdcommunity import AmdCommunity
 # Initialize the AmdCommunity class
 amd_community = AmdCommunity()
 
+
+
+def write_json_to_temp_file(data, directory):
+  try:
+    with tempfile.NamedTemporaryFile(dir=directory, mode='w+', delete=True) as temp_file:
+      json.dump(data, temp_file, indent=4)
+      temp_file.flush()
+      return temp_file.name
+  except OSError as e:
+    print(f"Error creating temporary file in {directory}: {e}")
+    return None
+
+
+
+
 # Streamlit app
 st.title("Testing")
+if(os.path.exists("./temp.json")):
+    st.text("file does not exists")
 
 # Text input for URL
 url = st.text_input("Enter the AMD Community URL:", "")
 # Process the URL and display the result
-if(os.path.exists("./temp.json")):
-    st.log("file does not exists")
+
 if st.button("Get Data"):
     if url.strip():
         try:
@@ -27,9 +44,13 @@ if st.button("Get Data"):
             
             # Display the JSON result
             st.code(json.dumps(result, indent=4), language="json")
-            with open("./temp.txt","w") as f:
-                json.dump(result,f,indent=3)
-            st.log("File now exists")
+            write_json_to_temp_file(result,"./temp.json")
+            st.text("File successfully saved")
+            temp = None
+            with open("./temp.json","r") as f:
+               temp = json.load(f)
+            st.code(temp,language="json")
+               
         except Exception as e:
             st.error(f"Error processing the URL: {e}")
     else:
