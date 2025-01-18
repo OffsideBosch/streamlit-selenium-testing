428c213af136ccb648621abbbba8a06a0798aaa8
OffsideBosch <mroffsidebosch@gmail.com>
2025-01-18 13:13:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ st.title("Testing")
 # Text input for URL
 url = st.text_input("Enter the AMD Community URL:", "")
 # Process the URL and display the result
+if(os.path.exists("./temp.txt")):
+    st.code("file does not exists",language="python")
 if st.button("Get Data"):
     if url.strip():
         try:
@@ -25,6 +27,9 @@ if st.button("Get Data"):
             
             # Display the JSON result
             st.code(json.dumps(result, indent=4), language="json")
+            with open("./temp.txt","w") as f:
+                f.write(result)
+            st.code("path exists",language="python")
         except Exception as e:
             st.error(f"Error processing the URL: {e}")
     else:
